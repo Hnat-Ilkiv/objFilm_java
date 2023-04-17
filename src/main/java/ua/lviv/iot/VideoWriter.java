9f61acb4cd8ce5f58f66941edef31187065d7148
Vikli <hnat.ilkiv.ir.2022@lpnu.ua>
2023-04-17 18:25:52 +0300

    delete commit from VideoWriter.java

--- src/main/java/ua/lviv/iot/VideoWriter.java
+++ src/main/java/ua/lviv/iot/VideoWriter.java
@@ -28,29 +28,5 @@ public class VideoWriter {
             }
             return fileName;
         }
-//        try {
-//            File myFile = new File("films_manager.csv");
-//            if (myFile.createNewFile()) {
-//                System.out.println("File created: " + myFile.getName());
-//            } else {
-//                System.out.println("File already exists.");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            FileWriter myWriter = new FileWriter("films_manager.csv");
-//            myWriter.write(video.getHeaders() + "\n" + video.toCSV() + "\n");
-//            myWriter.write(video1.getHeaders() + "\n" + video1.toCSV() + "\n");
-//            myWriter.write(video2.getHeaders() + "\n" + video2.toCSV() + "\n");
-//            myWriter.write(video3.getHeaders() + "\n" + video3.toCSV() + "\n");
-//            myWriter.close();
-//            System.out.println("Successfully wrote to the file.");
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
     }
 }
